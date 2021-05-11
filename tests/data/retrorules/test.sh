bc05eb08175c7e257e7bcd9a599abf54eb5991ed
breakthewall <joan.herisson@univ-evry.fr>
2021-05-11 14:05:55 +0200

    test: add log file

--- tests/data/retrorules/test.sh
+++ tests/data/retrorules/test.sh
@@ -8,7 +8,8 @@ fi
 
 datadir='tests/data/retrorules'
 file=$1
+log_file=log.$(basename $1)
 
 while read -r line; do
-    python -m rxn_rebuild --log_file log.$1 "$(echo $line | cut -f1 -d ' ')" "$(echo $line | cut -f2 -d ' ')";
+    python -m rxn_rebuild --log_file $log_file "$(echo $line | cut -f1 -d ' ')" "$(echo $line | cut -f2 -d ' ')";
 done < <(python $datadir/test.py $file)
